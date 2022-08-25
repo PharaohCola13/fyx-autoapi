622a931c1e05e21907247bc5c8d3855cffae7bdf
PharaohCola13 <academic@sriley.dev>
2022-08-25 08:07:49 -0600

    implements saving api references to .rst file

--- README.md
+++ README.md
@@ -1,12 +1,118 @@
-# fyx-autodocumentation
+# fyx-autodoc
 
 This action generates in-code API documentation skeleton for R, Python, and Rust.
 
-## Example Usage
+## The Fyx Framework
 
+**File Level Tags**
+Tag | Description
+-----|--------------------------
+file | the file name 
+synopsis | description of the file's purpose 
+author | the author with email in the form AuthorName \<AuthorEmail\>
+
+**Function Level Tags**
+
+Tag | Description
+-----|--------------------------
+detail | description of the function 
+param (type) arg (default) | the description of the argument
+return (type) | Information regarding the output of the function
+
+## Example Usage
 ```
 uses: PharaohCola13/fyx-autodocumentation@main
 env:
    FYX_USERNAME: PharaohCola13
    FYX_EMAIL: academic@sriley.dev
+```
+
+## Test case (test.py)
+
+Simple Python file
+```Python
+def test(arg6, arg7=1, arg8="test1"):
+    return out
+```
+Result
+```Python
+#> file:  ./test.py
+#> synopsis: 
+#> author: PharaohCola13 <academic@sriley.dev>
+def test(arg6, arg7=1, arg8="test1"):
+#> detail: 
+#> param (type) arg6:
+#> param (type) arg7 (1):
+#> param (type) arg8 (test1):
+#> return (type): 
+    return out
+```
+
+## Test case (test.r)
+
+Simple Python file
+```R
+test1 <- function(arg1, arg2="test2"){
+    return(args)
+}
+
+test2 <- function(arg3, arg4){
+   return(args)
+}
+```
+Result
+```R
+#> file:  ./test.r
+#> synopsis: 
+#> author: PharaohCola13 <academic@sriley.dev>
+test1 <- function(arg1, arg2="test2"){
+#> detail: 
+#> param (type) arg1:
+#> param (type) arg2 (test2):
+#> return (type): 
+    return(args)
+}
+
+test2 <- function(arg3, arg4){
+#> detail: 
+#> param (type) arg3:
+#> param (type) arg4:
+#> return (type): 
+   return(args)
+}
+```
+
+## Test case (test.rs)
+
+Simple Rust file
+```Rust
+fn test -> function(arg5:u32, arg6:i16){
+    return out
+}
+
+
+fn test2 -> function(arg5:u32, arg10:i8){
+}
+```
+Result
+```Rust
+//> file:  ./test.rs
+//> synopsis: 
+//> author: PharaohCola13 <academic@sriley.dev>
+fn test -> function(arg5:u32, arg6:i16){
+//> detail: 
+//> param (u32) arg5:
+//> param (i16) arg6:
+//> return (type): 
+    return out
+}
+
+
+fn test2 -> function(arg5:u32, arg10:i8){
+//> detail: 
+//> param (u32) arg5:
+//> param (i8) arg10:
+//> return (type): 
+
+}
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,28 @@
-#> file: ./autodoc.sh
+#!/bin/bash
+
+#> file: ./entrypoint.sh
 #> synopsis: Generates skeleton docstrings for code
-#> author: PharaohCola13 <academic@sriley.dev
+#> author: PharaohCola13 <academic@sriley.dev>
+
+WDIR=$(pwd)
+IGNORE=$(echo $WDIR/.ignore)
+
+IGSTR=""
+if [[ -e $IGNORE ]]; then
+    while read -r line; do
+        for i in $line; do
+            IGSTR+="-not -regex \"$WDIR/$line.*\" "
+        done
+    done < "$IGNORE"
+fi
+
+OUT=$(eval "find $WDIR $IGSTR -print")
+DIR=($OUT)
+unset DIR[0]
+
 
 DETAIL="detail: "
 RETURN="return (type): "
-
 
 FILE="file: "
 SYNOPSIS="synopsis: "
@@ -18,19 +36,19 @@ topwrite() {
         sed -i "2i$1> $SYNOPSIS" $2
     fi
     if [ $(cat $2 | grep -c "$AUTHOR") -eq 0 ]; then
-        AUTHOR="$1> author: $FYX_USERNAME <$FYX_EMAIL>"
-        sed -i "3i$AUTHOR" $2
+        sed -i "3i$1> $AUTHOR $FYX_USERNAME <$FYX_EMAIL>" $2
     fi
 }
 
-for i in ./*; do
+for i in ${DIR[@]}; do
     j=${i##*/}
     case ${j#*.} in 
         'r' | 'py')
             topwrite '#' $i
             while read -r line; do
-                case $line in 
-                *"function"* | *"def"*)
+                lzw=$(echo -e "$line" | tr -d '[:space:]')
+                case $lzw in 
+                *"<-function"* | $"def"*)
                     A=$(echo $line | cut -d "(" -f2 | cut -d ")" -f1)
                     IFS=',' read -a array <<< "$A"
 
@@ -51,7 +69,6 @@ for i in ./*; do
                         fi
 
                         if [[ -z $(sed -n "$N,$M{/$ARGS/{=;p}}" $i) ]]; then
-                            echo "$ARGS"
                             sed -i "/$line/a #> $ARGS" $i
                         fi
                     done
@@ -66,7 +83,7 @@ for i in ./*; do
             topwrite '//' $i
             while read -r line; do
                 case $line in 
-                *"function"*)
+                $"fn"*)
                     A=$(echo $line | cut -d "(" -f2 | cut -d ")" -f1)
                     IFS=',' read -a array <<< "$A"
 
@@ -94,11 +111,56 @@ for i in ./*; do
                 esac
             done < "$i"
             ;;  
-        "sh")
-            topwrite '#' $i
-            ;;
+        # "sh")
+        #     topwrite '#' $i
+        #     ;;
         *)
             continue
             ;;
     esac
+done
+
+fname="./api-reference.rst"
+repeat(){
+    for ((i = 0; i < $2; i++)); do echo -n "$1"; done
+}
+echo -e "**************\nAPI Reference\n**************\n" > $fname
+
+for i in ${DIR[@]}; do
+    input=$i
+    j=${i##*/}
+    echo -e "\n$(repeat "-" ${#j})\n$j\n$(repeat "-" ${#j})" >> $fname
+    while read -r line; do
+        lzw=$(echo -e "$line" | tr -d '[:space:]')
+        case $lzw in
+        *">module:"* | \
+        *">synopsis:"* | \
+        *">detail:"* | \
+        *">param"* | \
+        *">todo:"* | \
+        *">return"*)
+            Line=$(echo $line | cut -d '>' -f2 | sed "s/^[ \t]*//")
+            echo -e "\t${Line}" >> $fname
+            ;;
+        *"<-function"*)
+            func=${lzw/<-function/}
+            func=${func/{/}
+            func=$(echo -e "${func}" | tr -d '[:space:]')
+            echo -e "\n.. function:: ${func}\n" >> $fname
+        ;;
+        $"def"* ) 
+            func=${lzw/def/}
+            func=${func/:/}
+            func=$(echo -e "${func}" | tr -d '[:space:]')
+            echo -e "\n.. function:: ${func}\n" >> $fname
+        ;;
+        $"fn"*)
+            func=${lzw/->function/}
+            func=${func/fn/}
+            func=${func/{/}
+            func=$(echo -e "${func}" | tr -d '[:space:]')
+            echo -e "\n.. function:: ${func}\n" >> $fname
+        ;;
+        esac
+    done < "$input"
 done
